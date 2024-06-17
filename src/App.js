28bd662fe5a35e9d13b24170d8c4df7091eac3b2
ayeshaAhmed123 <i202424@nu.edu.pk>
2024-06-17 02:20:42 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./App.css"; // Assuming Tailwind CSS is imported here
+import Navbar from "./Components/Navbar";
+import Overview from "./Components/Overview";
+import BackgroundImage from "./Components/BackgroundImage";
+import Features from "./Components/Features";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      id="main "
+      className="m-0 p-0 box-border font-sans z-0 "
+      style={{
+        backgroundColor: "#0b0e0f",
+      }}
+    >
+      <div
+        id="sub-main  "
+        className=" box-border flex flex-col flex-wrap justify-start items-center content-center overflow-hidden relative min-h-screen "
+        style={{
+          "--rem": "16",
+          fontSize: "12px",
+          fontFamily: "sans-serif",
+          "--token-a8e28c9d-4ce6-4ad2-96a7-a1a12837bc97": "rgb(254, 232, 76)",
+          "--token-b86e4be1-9c71-4182-8aae-893747175d3f": "rgb(23, 30, 32)",
+          "--framer-aspect-ratio-supported": "auto",
+          WebkitFontSmoothing: "inherit",
+          gap: "0px",
+          padding: "100px 0 0",
+
+          backgroundColor: "#0b0e0f",
+          width: "auto",
+        }}
+      >
+        <Navbar />
+
+        <Overview />
+        <Features />
+        <BackgroundImage />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        customYellow: "rgb(254, 232, 76)",
+        customGray: "rgb(23, 30, 32)",
+        customDividerColor: "rgba(255, 255, 255, 0.3)",
+        textNav: "rgb(255, 255, 255)",
+        waitlistTest: "rgb(0,153,255)",
+      },
+      width: {
+        "1px": "1px",
+      },
+      height: {
+        "25px": "25px",
+      },
+      maxWidth: {
+        "100px": "110px",
+        "170px": "170px",
+        "210px": "210px",
+      },
+      fontFamily: {
+        sans: [
+          "Plus Jakarta Sans",
+          "Plus Jakarta Sans Placeholder",
+          "sans-serif",
+        ],
+      },
+      fontSize: {
+        "15px": "15px",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    // Other Tailwind CSS plugins
+  ],
+};
